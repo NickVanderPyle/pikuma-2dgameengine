cafbe8cc9d2962325fe0ebffbb95f5b010157f46
Nick V <NickVanderPyle@users.noreply.github.com>
2021-10-10 18:21:32 -0400

    Move the texture as if it were a player.

--- src/Game.cpp
+++ src/Game.cpp
@@ -2,6 +2,10 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <glm/glm.hpp>
+
+glm::vec2 playerPosition;
+glm::vec2 playerVelocity;
 
 Game::Game()
 {
@@ -81,11 +85,16 @@ void Game::ProcessInput()
 	}
 }
 
-void Game::Setup(){}
+void Game::Setup(){
+	playerPosition = glm::vec2(10.0, 20.0);
+	playerVelocity = glm::vec2(0.5, 0.0);
+}
 
 void Game::Update()
 {
 	std::cout << "update" << std::endl;
+	playerPosition.x += playerVelocity.x;
+	playerPosition.y += playerVelocity.y;
 }
 
 void Game::Render()
@@ -98,7 +107,12 @@ void Game::Render()
 	SDL_Surface* surface = IMG_Load("./assets/images/tank-tiger-right.png");
 	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
 	SDL_FreeSurface(surface);
-	SDL_Rect dstRect = {10, 10, 32, 32};
+	SDL_Rect dstRect = {
+		static_cast<int>(playerPosition.x),
+		static_cast<int>(playerPosition.y),
+		32,
+		32
+	};
 	SDL_RenderCopy(renderer, texture, NULL, &dstRect);
 	SDL_DestroyTexture(texture);
 
