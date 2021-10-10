43fa6480b50e2e5eb3f2dbfb6a193400b1c28d6a
Nick V <NickVanderPyle@users.noreply.github.com>
2021-10-10 16:58:56 -0400

    fixed size.

--- src/Game.cpp
+++ src/Game.cpp
@@ -23,8 +23,8 @@ void Game::Initialize()
 
 	SDL_DisplayMode displayMode;
 	SDL_GetCurrentDisplayMode(0, &displayMode);
-	windowWidth = displayMode.w;
-	windowHeight = displayMode.h;
+	windowWidth = 800; //displayMode.w;
+	windowHeight = 600; //displayMode.h;
 
 	window = SDL_CreateWindow(
 		NULL,
