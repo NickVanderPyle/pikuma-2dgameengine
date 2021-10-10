537bfcd5ee78b7fc9f2600abcc8caa2c1d91c4b5
Nick V <NickVanderPyle@users.noreply.github.com>
2021-10-09 20:59:01 -0400

    SOL/lua from https://github.com/ThePhD/sol2/releases

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,11 @@
 #include <SDL_mixer.h>
 #include <glm/glm.hpp>
 #include <imgui/imgui.h>
+#include <sol/sol.hpp>
 
 int main()
 {
+	sol::state lua;
 	SDL_Init(SDL_INIT_EVERYTHING);
 
 	glm::vec2  velocity = glm::vec2(5.0, -2.0);
