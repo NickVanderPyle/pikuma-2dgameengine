a0b2f1bf63aecd25ff4c81ef812cbc76b42eaf83
nick <NickVanderPyle@users.noreply.github.com>
2021-12-23 17:08:32 -0600

    Can exchange data with Lua.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
 #include "Game/Game.h"
+#include <sol/sol.hpp>
+#include <iostream>
+
+void TestLua() {
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
+
+    lua.script_file("./assets/scripts/myscript.lua");
+
+    int some_variableInCpp = lua["some_variable"];
+    std::cout << "The value of some_variable in c++ is " << some_variableInCpp << std::endl;
+}
 
 int main(int argc, char *argv[]) {
-    Game game;
+//    Game game;
+//
+//    game.Initialize();
+//    game.Run();
+//    game.Destroy();
 
-    game.Initialize();
-    game.Run();
-    game.Destroy();
+    TestLua();
 
     return 0;
 }
